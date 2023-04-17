92cc6c004b73ffbb615119516a9c7e724b47baa7
Jayesh Saini <jayesh.saini.95@gmail.com>
2023-04-16 22:48:10 -0400

    added customer jsp

--- src/main/java/com/js/azure/AzureApplication.java
+++ src/main/java/com/js/azure/AzureApplication.java
@@ -14,6 +14,38 @@ public class AzureApplication {
 	    String htmlContent = "<html><body><h1>Congrats</h1><p>You have successfully deployed your Spring Boot application to Azure.</p></body></html>";
 	    return htmlContent;
 	}
+	
+	@GetMapping("/Customer")
+	public String registerCustomer() {
+		String htmlContent = "<html>\n"
+				+ "<head>\n"
+				+ "<meta charset=\"ISO-8859-1\">\n"
+				+ "<title>Customer</title>\n"
+				+ "<link rel=\"stylesheet\" type=\"text/css\" href=\"styles/style.css\">\n"
+				+ "</head>\n"
+				+ "<body>\n"
+				+ "	<h1>Customer</h1>\n"
+				+ "	<h3>Register Customer</h3>\n"
+				+ "	<form action=\"PatientCreate\" method=\"post\">\n"
+				+ "		<label>Name: </label> <input type=\"text\" name=\"name\" /> <br> \n"
+				+ "		<label>Age:	</label> <input type=\"text\" name=\"age\" /> <br> \n"
+				+ "		\n"
+				+ "		<label>Mode: </label> <input type=\"text\" name=\"mode\" /> <br> \n"
+				+ "		<label>Destination: </label> <input type=\"text\" name=\"dest\" /> <br> \n"
+				+ "		<label>Origin: </label> <input type=\"text\" name=\"origin\" /> <br> \n"
+				+ "		<label>Date: </label> <input type=\"date\" name=\"date\" /> <br> \n"
+				+ "		<input type=\"submit\" value=\"Book Itinerary\" />\n"
+				+ "	</form>\n"
+				+ "\n"
+				+ "\n"
+				+ "	<form method=\"post\" action=\"Travel.jsp\">\n"
+				+ "		<button type=\"submit\">Show Bookings</button>\n"
+				+ "	</form>\n"
+				+ "\n"
+				+ "</body>\n"
+				+ "</html>";
+		return htmlContent;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureApplication.class, args);
