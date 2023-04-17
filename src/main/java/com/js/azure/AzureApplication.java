21a74ae43bbcef31b1ffdcbe9ff1b82d1b99e79d
Jayesh Saini <jayesh.saini.95@gmail.com>
2023-04-16 22:24:16 -0400

    initial commit

---
+++ src/main/java/com/js/azure/AzureApplication.java
@@ -0,0 +1,20 @@
+package com.js.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class AzureApplication {
+	
+	@GetMapping("/message")
+	public String message() {
+	    String htmlContent = "<html><body><h1>Congrats</h1><p>You have successfully deployed your Spring Boot application to Azure.</p></body></html>";
+	    return htmlContent;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureApplication.class, args);
+	}
+
+}
